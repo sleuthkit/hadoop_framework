3b9245952ce0bb1e82fb0e661f0ca9a32d7a961c
ihelmke <ihelmke@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-04-14 19:23:51 +0000

    TXPETE-4 Basic java pipeline code
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@9641 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ pipeline/src/main/java/org/sleuthkit/hadoop/pipeline/Pipeline.java
@@ -0,0 +1,49 @@
+package org.sleuthkit.hadoop.pipeline;
+
+import org.sleuthkit.hadoop.GrepSearchJob;
+import org.sleuthkit.hadoop.TokenizeAndVectorizeDocuments;
+
+public class Pipeline {
+    
+    // The storage place of raw files containing:
+    
+    // content: raw file content bytes
+    public static final String TIKA_IN_DIR  = "hdfs://localhost/texaspete/raw/";
+    
+    // The storage place of sequencefiles containing:
+    // key:   file name
+    // value: file content (plain text)
+    public static final String TIKA_OUT_DIR = "hdfs://localhost/texaspete/text/";
+
+    // The storage place of sequencefiles containing:
+    // key:   file name
+    // value: keyword vector (should be a set)
+    public static final String GREP_OUT_DIR = "hdfs://localhost/texaspete/grepped/";
+    
+    // A file containing lines of text, each of which represents a regex.
+    public static final String GREP_KEYWORDS = "hdfs://localhost/texaspete/regexes";
+    
+    // The storage place of sequencefiles containing:
+    // key:   file name
+    // value: tokenized file content (vector of words)
+    public static final String TOKEN_OUT_DIR = "hdfs://localhost/texaspete/tokens";
+    
+    // The storage place of sequencefiles containing:
+    // key:   file name
+    // value: term frequency vectors.
+    // This directory will likely also contain a dictionary vector mapping
+    // the indices in the sparse TF vectors to words.
+    public static final String TF_DIR = "hdfs://localhost/texaspete/vectors";
+    
+    public static void main(String[] argv) {
+        // TODO: Run the TIKA code here, collecting content from text files.
+        
+        // Run the GREP search code.
+
+        GrepSearchJob.runPipeline(TIKA_OUT_DIR, GREP_OUT_DIR, GREP_KEYWORDS);
+        
+        TokenizeAndVectorizeDocuments.runPipeline(TIKA_OUT_DIR, TOKEN_OUT_DIR, TF_DIR);
+        
+        
+    }
+}
