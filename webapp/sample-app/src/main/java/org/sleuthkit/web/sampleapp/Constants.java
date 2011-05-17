fd7b6d01a024896c0aef12dae836a003063b0a33
mzand <mzand@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-05-17 14:02:38 +0000

    take 2 on checking in the webapp
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@11027 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ webapp/sample-app/src/main/java/org/sleuthkit/web/sampleapp/Constants.java
@@ -0,0 +1,17 @@
+package org.sleuthkit.web.sampleapp;
+
+public class Constants {
+	
+	/****************************************************/
+	/* LITERALS 										*/
+	/****************************************************/
+	public static final String HOME_PAGE_TITLE = "Welcome to Texas Pete web application";
+	public static final String HOME_PAGE_INSTRUCTIONS = "Set Command pattern, select an image file on the server and assign an id to it. Then Execute Command remotely and see results.";
+	public static final String HOME_PAGE_CP_LABEL = "Command pattern: ";
+	public static final String HOME_PAGE_FILE_LABEL = "Image File: "; 
+	public static final String HOME_PAGE_ID_LABEL = "Image File Id: "; 
+	
+	public static final String LOGIN_PAGE_TITLE = "Texas Pete web application - Login";
+	public static final String LOGIN_USERNAME_LABEL = "Username:";
+	public static final String LOGIN_PASSWORD_LABEL = "Password:";
+}
