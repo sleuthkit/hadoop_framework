e356ea11d2eed8151f26cc69732aeb9a3b738d31
joel <joel@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-05 12:28:01 +0000

    Display the unparseable string in the exception.
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@12068 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/main/java/com/lightboxtechnologies/spectrum/JsonWritable.java
+++ core/src/main/java/com/lightboxtechnologies/spectrum/JsonWritable.java
@@ -45,7 +45,7 @@ public class JsonWritable implements Writable {
       return getData();
     }
     catch (IOException e) {
-      throw new RuntimeException(e);
+      throw new RuntimeException("Bad JSON: \"" + Json + '"', e);
     }
   }
 
