c710481cde611932aac14296f2f5c48dd282cb8e
geoff <geoff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-10 04:52:59 +0000

    Added allocated space to driveInfo, added tooltip to drive layout
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@12360 e6edf6fb-f266-4316-afb4-e53d95876a76

--- reports/data/index.js
+++ reports/data/index.js
@@ -1,30 +1,30 @@
 var deviceInfo = [
-  {a: "deviceName", n: "AR-2011-03-B438490"},
-  {a: "md5", n: "E4D909C290D0FB1CA068FFADDF22CBD0"},
-  {a: "sha1", n: "DE9F2C7F D25E1B3A FAD3E85A 0BD17D9B 100DB4B3"},
-  {a: "loadedBy", n: "John Q. Public"},
-  {a: "loadedOn", n: "2011-03-30T09:40:13Z"},
-  {a: "totalFiles", n: 108342},
-  {a: "knownFiles", n: 12379},
-  {a: "extractedFiles", n: 1276},
-  {a: "size", n: 249978682486},
-  {a: "volumeList", n:
-	  [ {a: "Restore", n: 10289322378, fs: "FAT32"},
-	  	{a: "System", n: 58256345901, fs: "NTFS"},
-	  	{a: "Data", n: 159346239234, fs: "NTFS"}
+  {"a": "deviceName", "n": "AR-2011-03-B438490"},
+  {"a": "md5", "n": "E4D909C290D0FB1CA068FFADDF22CBD0"},
+  {"a": "sha1", "n": "DE9F2C7F D25E1B3A FAD3E85A 0BD17D9B 100DB4B3"},
+  {"a": "loadedBy", "n": "John Q. Public"},
+  {"a": "loadedOn", "n": "2011-03-30T09:40:13Z"},
+  {"a": "totalFiles", "n": 108342},
+  {"a": "knownFiles", "n": 12379},
+  {"a": "extractedFiles", "n": 1276},
+  {"a": "size", "n": 1249978682486},
+  {"a": "volumeList", "n":
+	  [ {"a": "Restore", "n": 10289322378, "alloc": 8368356783, "fs": "FAT32"},
+	  	{"a": "System", "n": 58256345901, "alloc": 25935712034, "fs": "NTFS"},
+	  	{"a": "Data", "n": 1159346239234, "alloc": 114572194063, "fs": "NTFS"}
 	  ]
 	}
 ];
 
 var deviceFileExtensions = [
-  {a: "docx", n: 1299},
-  {a: "avi", n: 1173},
-  {a: "mp3", n: 1025},
-  {a: "xlsx", n: 985},
-  {a: "txt", n: 638},
-  {a: "zip", n: 603},
-  {a: "pptx", n: 579},
-  {a: "mkv", n: 543},
-  {a: "3gp", n: 458},
-  {a: "amr", n: 132}
+  {"a": "docx", "n": 1299},
+  {"a": "avi", "n": 1173},
+  {"a": "mp3", "n": 1025},
+  {"a": "xlsx", "n": 985},
+  {"a": "txt", "n": 638},
+  {"a": "zip", "n": 603},
+  {"a": "pptx", "n": 579},
+  {"a": "mkv", "n": 543},
+  {"a": "3gp", "n": 458},
+  {"a": "amr", "n": 132}
 ];
