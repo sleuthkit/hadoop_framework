d5b5d15bab9bcd478019336d3c7a66e30255029e
jon <jon@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-22 19:33:56 +0000

    Resolved conflict
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@12864 e6edf6fb-f266-4316-afb4-e53d95876a76

--- pipeline/src/main/java/org/sleuthkit/hadoop/pipeline/Pipeline.java
+++ pipeline/src/main/java/org/sleuthkit/hadoop/pipeline/Pipeline.java
@@ -1,3 +1,4 @@
+
 /*
    Copyright 2011 Basis Technology Corp.
 
@@ -25,38 +26,39 @@ import org.sleuthkit.hadoop.SequenceFsEntryText;
 import org.sleuthkit.hadoop.TokenizeAndVectorizeDocuments;
 
 import com.lightboxtechnologies.spectrum.HBaseTables;
+import com.lightboxtechnologies.spectrum.HDFSArchiver;
 
 public class Pipeline {
-    // A file containing lines of text, each of which represents a regex.
-    public static final String GREP_KEYWORDS = "/texaspete/regexes";
+  // A file containing lines of text, each of which represents a regex.
+  public static final String GREP_KEYWORDS = "/texaspete/regexes";
 
-    public static void main(String[] argv) throws Exception {
-        if (argv.length != 2) {
-          System.err.println("Usage: Pipeline image_id friendly_name");
-          System.exit(1);
-        }
-        final String imageID = argv[0];
-        final String friendlyName = argv[1];
-        final String prefix = "/texaspete/data/" + imageID;
-        final String seqDumpDirectory = prefix + "/text";
-        final String tokenDumpDirectory = prefix + "/tokens";
-        final String vectorDumpDirectory = prefix + "/vectors";
-        final String clusterDumpDirectory = prefix + "/clusters";
-        final String dictionaryDumpDirectory = prefix + "/vectors/dictionary.file-0";
-
-        FSEntryTikaTextExtractor.runPipeline(HBaseTables.ENTRIES_TBL, imageID, friendlyName);
-        GrepSearchJob.runPipeline(HBaseTables.ENTRIES_TBL, imageID, GREP_KEYWORDS, friendlyName);
-        
-        // This will allow us to only cluster if we have documents that are written out to sequence files.
-        // In other words, clustering will only take place if there are things TO cluster.
-        boolean filesToSequence = (SequenceFsEntryText.runPipeline(seqDumpDirectory, imageID, friendlyName));
-        if (filesToSequence) {
-            TokenizeAndVectorizeDocuments.runPipeline(seqDumpDirectory, tokenDumpDirectory, vectorDumpDirectory);
-        }
-        
-        GrepReportGenerator.runPipeline(GREP_KEYWORDS, imageID, friendlyName);
-
-        ClusterDocuments.runPipeline(vectorDumpDirectory + "/tfidf-vectors/", clusterDumpDirectory, dictionaryDumpDirectory, .65, .65, imageID, friendlyName);
+  public static void main(String[] argv) throws Exception {
+    if (argv.length != 2) {
+      System.err.println("Usage: Pipeline image_id friendly_name");
+      System.exit(1);
     }
+    final String imageID = argv[0];
+    final String friendlyName = argv[1];
+    final String prefix = "/texaspete/data/" + imageID;
+    final String seqDumpDirectory = prefix + "/text";
+    final String tokenDumpDirectory = prefix + "/tokens";
+    final String vectorDumpDirectory = prefix + "/vectors";
+    final String clusterDumpDirectory = prefix + "/clusters";
+    final String dictionaryDumpDirectory = prefix + "/vectors/dictionary.file-0";
 
+    FSEntryTikaTextExtractor.runPipeline(HBaseTables.ENTRIES_TBL, imageID, friendlyName);
+    GrepSearchJob.runPipeline(HBaseTables.ENTRIES_TBL, imageID, GREP_KEYWORDS, friendlyName);
+    SequenceFsEntryText.runTask(seqDumpDirectory, imageID, friendlyName);
+
+    // This will allow us to only cluster if we have documents that are written out to sequence files.
+    // In other words, clustering will only take place if there are things TO cluster.
+    boolean filesToSequence = (SequenceFsEntryText.runPipeline(seqDumpDirectory, imageID, friendlyName));
+    if (filesToSequence) {
+      TokenizeAndVectorizeDocuments.runPipeline(seqDumpDirectory, tokenDumpDirectory, vectorDumpDirectory);
+    }
+    GrepReportGenerator.runPipeline(GREP_KEYWORDS, imageID, friendlyName);
+    ClusterDocuments.runPipeline(vectorDumpDirectory + "/tfidf-vectors/", clusterDumpDirectory, dictionaryDumpDirectory, .65, .65, imageID, friendlyName);
+
+    HDFSArchiver.runPipeline(prefix + "/reports", prefix + "/reports.zip");
+  }
 }
