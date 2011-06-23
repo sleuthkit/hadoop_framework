4b6ac6e1164c5e8e88a8ee20ca3a0b6dc96f8570
geoff <geoff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-23 02:13:40 +0000

    Renamed graphics.js to graphicsclusters.js
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@12898 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ reports/data/graphicsclusters.js
@@ -0,0 +1,14 @@
+var imgClusterData = [
+  {name: "Group 01", x: 80, y: 12, z: 96, cf: 20},
+	{name: "Group 02", x: 57, y: 77, z: 88, cf: 75},
+  {name: "Group 03", x: 49, y: 60, z: 73, cf: 50},
+  {name: "Group 04", x: 61, y: 89, z: 70, cf: 80},
+  {name: "Group 05", x: 9, y: 19, z: 64, cf: 75},
+  {name: "Group 06", x: 15, y: 19, z: 52, cf: 40},
+  {name: "Group 07", x: 19, y: 22, z: 42, cf: 55},
+  {name: "Group 08", x: 5, y: 85, z: 31, cf: 25},
+  {name: "Group 09", x: 64, y: 12, z: 27, cf: 90},
+  {name: "Group 10", x: 31, y: 65, z: 25, cf: 15},
+  {name: "Group 11", x: 73, y: 94, z: 22, cf: 60},
+  {name: "Group 12", x: 94, y: 29, z: 13, cf: 65}
+];
