39c04ad744386e55c52c79b6037358061c77a2b8
geoff <geoff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-26 04:48:17 +0000

    Renamed menu items
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@13005 e6edf6fb-f266-4316-afb4-e53d95876a76

--- reports/js/menuList.js
+++ reports/js/menuList.js
@@ -21,7 +21,7 @@ var menuList = [
   {a: "Search Hits", link: "searchhits.html"},
   {a: "Entities", link: "entities.html"},
   {a: "Known Bad", link: "knownbad.html"},
-  {a: "Documents", link: "documents.html"},
+  {a: "Document Clusters", link: "documents.html"},
   {a: "Graphics", link: "graphics.html"},
-  {a: "Link Analysis", link: "linkanalysis.html"}
+  {a: "Similar Devices", link: "linkanalysis.html"}
 ];
