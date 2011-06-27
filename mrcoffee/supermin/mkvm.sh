cfaa6fd72379ab90610de5bd021fef019ceb23ee
joel <joel@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-27 08:45:18 +0000

    Added socat package for testing.
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@13030 e6edf6fb-f266-4316-afb4-e53d95876a76

--- mrcoffee/supermin/mkvm.sh
+++ mrcoffee/supermin/mkvm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir supermin.d
-febootstrap --names bash coreutils udev -o supermin.d
+febootstrap --names bash coreutils udev socat -o supermin.d
 echo init | cpio -o -H newc --quiet > supermin.d/init.img
 febootstrap-supermin-helper -f ext2 supermin.d x86_64 kernel initrd root
 
