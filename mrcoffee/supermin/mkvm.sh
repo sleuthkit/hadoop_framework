6418646068c03b8c903018f2f9eea834fdf408ee
joel <joel@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-27 08:45:22 +0000

    Added mrcoffee binary to appliance.
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@13031 e6edf6fb-f266-4316-afb4-e53d95876a76

--- mrcoffee/supermin/mkvm.sh
+++ mrcoffee/supermin/mkvm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -x
 mkdir supermin.d
 febootstrap --names bash coreutils udev socat -o supermin.d
 echo init | cpio -o -H newc --quiet > supermin.d/init.img
+echo mrcoffee | cpio -o -H newc --quiet >supermin.d/mrcoffee.img
 febootstrap-supermin-helper -f ext2 supermin.d x86_64 kernel initrd root
 
