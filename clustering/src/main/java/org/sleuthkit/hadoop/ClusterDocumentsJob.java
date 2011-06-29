dfb886e6d968b55e4c1918891dbfd9f54b75e5a4
ihelmke <ihelmke@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-29 20:12:49 +0000

    Removed old ClusterDocuments file. It has been renamed to ClusterDocumentsJob.
    * Added mahout comments to ClusterDocumentsJob.
    
    
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@13209 e6edf6fb-f266-4316-afb4-e53d95876a76

--- clustering/src/main/java/org/sleuthkit/hadoop/ClusterDocumentsJob.java
+++ clustering/src/main/java/org/sleuthkit/hadoop/ClusterDocumentsJob.java
@@ -42,42 +42,56 @@ public class ClusterDocumentsJob {
 
     public static void main(String[] argv) {
         if (argv.length < 5) {
-            System.out.println("Usage: ClusterDocuments <input_dir> <output_dir> <dictionary_file> <t1> <t2> <image_hash> <friendly_name>");
+            System.out.println("Usage: ClusterDocuments <input_dir> <output_dir> <dictionary_file> <t1> <t2> <image_hash> <friendly_name> <img_id>");
             System.exit(1);
         }
-        runPipeline(argv[0], argv[1], argv[2], .65, .65, argv[3], argv[4]);
+        runPipeline(argv[0], argv[1], argv[2], .65, .65, argv[3], argv[4], argv[5]);
     }
 
-    public static int runPipeline(String input, String output, String dictionary, double t1, double t2, String imageID, String friendlyName) {
+    /**
+     * Runs the clutering algorithms on the tfidf vectors that have been placed in
+     * sequence files in directory 'input', and puts raw cluster/json data in
+     * 'output'. Also puts json reporting data in the reports/data folder.
+     * @param input The sequence files to cluster on.
+     * @param output The output directory for raw canopy/kmeans cluster data.
+     * @param dictionary The dictionary vector which maps the indices of the vectors
+     * to words.
+     * @param t1 The t1 value for canopy clustering. The distance measure for
+     * canopy is CosineDistanceMeasure, so this should be a value between 0 and 1.
+     * @param t2 The t2 value for canopy clustering. Again, should be between
+     * t1 and 1. A smaller distance beween the two results in more clusters;
+     * a greater distance results in fewer.
+     * @param imageID The hash of the image.
+     * @param friendlyName The friendly, user given name of the image.
+     * @param baseDir The base directory where output data for this image
+     * is stored. Used to place the reporting data in the correct location.
+     * @return A status code; will be non-zero if the task failed.
+     */
+    public static int runPipeline(String input, String output, String dictionary, double t1, double t2, String imageID, String friendlyName, String baseDir) {
         Configuration conf = new Configuration();
         conf.set("mapred.child.java.opts", "-Xmx4096m");
-        try {
-            SKJobFactory.addDependencies(conf);
-        } catch (IOException e) {
-            LOG.error("Exception while adding dependencies.", e);
-        }
         Path canopyInputPath = new Path(input);
         Path canopyOutputPath = new Path(output + "/canopy");
 
         Path kmeansInputPath = new Path(input);
         Path kmeansOutputPath = new Path(output + "/kmeans");
-        // Canopy (AFAIK) only does one output pass, so the relevant
+        // Canopy (I'm quite certain) only does one pass, so the relevant
         // clusters should be found in this file. For KMeans, this may not
         // be the case. Note, though, that the final clusters with document
         // vectors will be in a different file.
         Path kmeansClusters = new Path(output + "/canopy/clusters-0");
 
-        // perform canopy clustering on the grep-matched docs first,
-        // to get some centroids for kmeans.
-        
-        
         try {
             CanopyDriver.run(conf, canopyInputPath, canopyOutputPath, new CosineDistanceMeasure(), t1, t2, true, false);
         } catch (Exception e) {
             LOG.error("Failure running mahout canopy.", e);
             return 1;
         }
-        // now run kmeans using those centroids.
+
+        // The convergencedelta and maxiterations affect how long kmeans will
+        // take to run and how many times we run the algorithm before we give
+        // up. The numbers we are using here seem to give reasonably good
+        // results.
         try {
             KMeansDriver.run(conf, kmeansInputPath, kmeansClusters, kmeansOutputPath, new CosineDistanceMeasure(), .5, 20, true, false);
         } catch (Exception e) {
@@ -90,7 +104,8 @@ public class ClusterDocumentsJob {
             // Output top cluster matches //
             ////////////////////////////////
             Job job = SKJobFactory.createJob(imageID, friendlyName, JobNames.OUTPUT_CLUSTER_MATCH);
-            job.setJarByClass(ClusterDocumentsJob.class);
+            job.setJarByClass(TopFeatureMapper.class);
+
 
             // Get the final kmeans iteration. This is sort of a pain but for
             // whatever reason hadoop has no mechanism to do this for us.
@@ -151,13 +166,14 @@ public class ClusterDocumentsJob {
             job.setOutputFormatClass(TextOutputFormat.class);
 
             job.waitForCompletion(true);
-            
+
             // Note that, since we limit the number of reduce tasks to 1, there should only be
             // one reduce 'part'.
+
             ClusterJSONBuilder.buildReport(
                     new Path(output + "/kmeans/topClusters/part-r-00000"), 
                     new Path(output + "/kmeans/jsonClusteredPoints/part-r-00000"),
-                    new Path("/texaspete/data/" + imageID +  "/reports/data/documents.js"));
+                    new Path(baseDir + "/reports/data/documents.js"));
             return 0;
         } catch (IOException ex) {
             LOG.error("Failure while performing HDFS file IO.", ex);            
@@ -170,4 +186,5 @@ public class ClusterDocumentsJob {
         return 3;
         
     }
+    
 }
