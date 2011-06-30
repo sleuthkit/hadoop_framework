987a2a62ba4e5f64ac0a22365b9c7fda29349a5f
joel <joel@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-30 15:22:08 +0000

    Removed unused imports.
    git-svn-id: https://fourtwosix.jira.com/svn/TXPETE/trunk@13248 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/main/java/com/lightboxtechnologies/spectrum/FsEntryHBaseInputFormat.java
+++ core/src/main/java/com/lightboxtechnologies/spectrum/FsEntryHBaseInputFormat.java
@@ -37,8 +37,6 @@ import org.apache.hadoop.hbase.mapreduce.TableInputFormatBase;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.io.*;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.*;
